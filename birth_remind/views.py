8bc189085f6d58f9cb64693055f6481db0c73fdb
Milea90 <>
2023-07-22 22:14:43 +0000

    Add a Django form

--- birth_remind/views.py
+++ birth_remind/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Item
+from .forms import ItemForm
 
 
 # Create your views here.
@@ -16,5 +17,15 @@ def get_birth(request):
 
 
 def add_birth(request):
+    if request.method == 'POST':
+        form = ItemForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('get_birth')
+
+        form = ItemForm()
+        context = {
+            'form': form
+        }
 
     return render(request, 'birth_remind/add_birthdays.html')
